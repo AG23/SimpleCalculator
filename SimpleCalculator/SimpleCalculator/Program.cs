60f6a83097ad17b44f5eb76e53d9bffbbd482409
AG23 <gladkikh23@yandex.ru>
2016-01-27 14:25:04 +0300

    add code

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -10,6 +10,32 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            int a, b;
+            int c;
+            Console.WriteLine("enter 1 number ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Choose action ");
+            Console.WriteLine("Example: +,-,/,*,%");
+            c = Console.ReadLine()[0];
+            Console.WriteLine("enter 2 number ");
+            b = int.Parse(Console.ReadLine());
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine("{0}+{1}={2}", a, b, ((float)a + b));
+                    break;
+                case '-':
+                    Console.WriteLine("{0}-{1}={2}", a, b, ((float)a - b));
+                    break;
+                case '/':
+                    Console.WriteLine("{0}/{1}={2}", a, b, ((float)a / b));
+                    break;
+                case '*':
+                    Console.WriteLine("{0}*{1}={2}", a, b, ((float)a * b));
+                    break;
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
