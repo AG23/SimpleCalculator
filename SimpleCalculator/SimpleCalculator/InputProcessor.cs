c23b369dbf1b33a3e61d8227ae50410cbce4e589
AG23 <gladkikh23@yandex.ru>
2016-02-04 13:08:22 +0300

    add class to new file

---
+++ SimpleCalculator/SimpleCalculator/InputProcessor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    internal class InputProcessor
+    {
+            public void Run()
+        {
+            while (true)
+            {
+                int a, b;
+                int c;
+                Console.WriteLine("enter 1 number ");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Choose action ");
+                Console.WriteLine("Example: +,-,/,*,%");
+                c = Console.ReadLine()[0];
+                Console.WriteLine("enter 2 number ");
+                b = int.Parse(Console.ReadLine());
+                switch (c)
+                {
+                    case '+':
+                        Console.WriteLine("{0}+{1}={2}", a, b, ((float)a + b));
+                        break;
+                    case '-':
+                        Console.WriteLine("{0}-{1}={2}", a, b, ((float)a - b));
+                        break;
+                    case '/':
+                        Console.WriteLine("{0}/{1}={2}", a, b, ((float)a / b));
+                        break;
+                    case '*':
+                        Console.WriteLine("{0}*{1}={2}", a, b, ((float)a * b));
+                        break;
+                }
+            }
+        }
+    }
+}
--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -13,40 +13,5 @@ namespace SimpleCalculator
             var processor = new InputProcessor();
             processor.Run(); 
         }
-    }
-
-    internal class InputProcessor
-    {
-        public void Run()
-        {
-            while (true)
-            {
-            int a, b;
-            int c;
-            Console.WriteLine("enter 1 number ");
-            a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Choose action ");
-            Console.WriteLine("Example: +,-,/,*,%");
-            c = Console.ReadLine()[0];
-            Console.WriteLine("enter 2 number ");
-            b = int.Parse(Console.ReadLine());
-            switch (c)
-            {
-                case '+':
-                    Console.WriteLine("{0}+{1}={2}", a, b, ((float)a + b));
-                    break;
-                case '-':
-                    Console.WriteLine("{0}-{1}={2}", a, b, ((float)a - b));
-                    break;
-                case '/':
-                    Console.WriteLine("{0}/{1}={2}", a, b, ((float)a / b));
-                    break;
-                case '*':
-                    Console.WriteLine("{0}*{1}={2}", a, b, ((float)a * b));
-                    break;
-            }
-            }
-          
+    }  
         }
-    }
-}
