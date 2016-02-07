8c583daae4e0310a84022132c682878076d4da3d
AG23 <gladkikh23@yandex.ru>
2016-02-07 13:57:24 +0300

    add try/catch

--- SimpleCalculator/SimpleCalculator/InputProcessor.cs
+++ SimpleCalculator/SimpleCalculator/InputProcessor.cs
@@ -5,36 +5,43 @@ namespace SimpleCalculator
 {
     internal class InputProcessor
     {
-            public void Run()
+        public void Run()
         {
             while (true)
             {
-                float a, b;
-                string c;
-                String s;
-                s = Console.ReadLine();
-                String[] exp = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                var operation = s[0];
-                var args = s.Skip(1);
-                a = float.Parse(exp[1]);
-                b = float.Parse(exp[2]);
-                c = exp[0];
-                switch (c)
+                float firstNumber, secondNumber;
+                string action;
+                String input;
+                try
                 {
-                    case "sum":
-                        Console.WriteLine("{0}+{1}={2}", a, b, (a + b));
-                        break;
-                    case "sub":
-                        Console.WriteLine("{0}-{1}={2}", a, b, (a - b));
-                        break;
-                    case "div":
-                        Console.WriteLine("{0}/{1}={2}", a, b, (a / b));
-                        break;
-                    case "mul":
-                        Console.WriteLine("{0}*{1}={2}", a, b, (a * b));
-                        break;
+                    input = Console.ReadLine();
+                    String[] exp = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    var operation = input[0];
+                    var args = input.Skip(1);
+                    firstNumber = float.Parse(exp[1]);
+                    secondNumber = float.Parse(exp[2]);
+                    action = exp[0];
+                    switch (action)
+                    {
+                        case "sum":
+                            Console.WriteLine("{0}+{1}={2}", firstNumber, secondNumber, (firstNumber + secondNumber));
+                            break;
+                        case "sub":
+                            Console.WriteLine("{0}-{1}={2}", firstNumber, secondNumber, (firstNumber - secondNumber));
+                            break;
+                        case "div":
+                            Console.WriteLine("{0}/{1}={2}", firstNumber, secondNumber, (firstNumber / secondNumber));
+                            break;
+                        case "mul":
+                            Console.WriteLine("{0}*{1}={2}", firstNumber, secondNumber, (firstNumber * secondNumber));
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please try again");
                 }
             }
         }
-        }
     }
+}
