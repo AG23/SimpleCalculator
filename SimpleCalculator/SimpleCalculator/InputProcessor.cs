d45ae96e08b4dce4853c4d358692fe32a9d4dd6a
AG23 <gladkikh23@yandex.ru>
2016-04-04 14:03:59 +0300

    add checking

--- SimpleCalculator/SimpleCalculator/InputProcessor.cs
+++ SimpleCalculator/SimpleCalculator/InputProcessor.cs
@@ -15,27 +15,49 @@ namespace SimpleCalculator
                 String[] exp = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                 var operation = exp[0];
                 var args = input.Skip(1);
+                float firstNumber, secondNumber;
                 string action;
                 action = exp[0];
-                OperationBase calculator;
-                switch (action)
+                if (action.Length == 3)
                 {
-                    case "sum":
-                        calculator = new SumOperation();
-                        calculator.Execute(exp.ToList<String>());
-                        break;
-                    case "sub":
-                        calculator = new SubOperation();
-                        calculator.Execute(exp.ToList<String>());
-                        break;
-                    case "div":
-                        calculator = new DivOperation();
-                        calculator.Execute(exp.ToList<String>());
-                        break;
-                    case "mul":
-                        calculator = new MulOperation();
-                        calculator.Execute(exp.ToList<String>());
-                        break;
+                    if (Single.TryParse(exp[1], out firstNumber))
+                    {
+                        if (Single.TryParse(exp[2], out secondNumber))
+                        {
+                            OperationBase calculator;
+                            switch (action)
+                            {
+                                case "sum":
+                                    calculator = new SumOperation();
+                                    calculator.Execute(exp.ToList<String>());
+                                    break;
+                                case "sub":
+                                    calculator = new SubOperation();
+                                    calculator.Execute(exp.ToList<String>());
+                                    break;
+                                case "div":
+                                    calculator = new DivOperation();
+                                    calculator.Execute(exp.ToList<String>());
+                                    break;
+                                case "mul":
+                                    calculator = new MulOperation();
+                                    calculator.Execute(exp.ToList<String>());
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please, check your second number");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please, check your first number");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter: sum, sub, mul or div");
                 }
             }
         }
